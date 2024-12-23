437cf7352c3a8fdc7e374b1efe36522ffc282551
Kichan-Seoul <kimkc30@icloud.com>
2024-12-23 16:03:25 +0900

    12.23 3교시

---
+++ chap01_print/src/chap01_print/Print01.java
@@ -0,0 +1,21 @@
+package chap01_print;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// main 저게 있어야 컴파일이 되고 메인 저거 밑에부터 컴파일이 됨
+		// 이건 주석인데 얘는 한줄 주석 이라고 하고 컴파일이 되지 않음(실행이 되지 않음)
+		/*이건 여러줄 주석
+		 * 이렇게 하면 여러줄 적어넣을수 있슴
+		 * 닫을땐 별 슬레시 하면 되는데 알아서 만들어지는거같음
+		 */
+		//한줄 주석의 단축기는 ctrl + / 이다
+		/* 여러줄 주석은 드래그 한 후에 ctrl + shift + / 를 하면 된다 */
+		System.out.println("본인이름 : 김기찬");
+		System.out.println("더조은 아카데미 학원");
+		System.out.println("아 클스마스 뭐하지");
+		
+	}
+
+}
---
+++ chap01_print/src/chap01_print/Print_String_03.java
@@ -0,0 +1,18 @@
+package chap01_print;
+
+public class Print_String_03 {
+
+	public static void main(String[] args) {
+		System.out.print("3+5=");
+		System.out.println("8");
+		
+		//연산은 쌍따음표("")를 넣지 않는다
+		System.out.println(3+5);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3);
+		//정수/정수=정수 : 몫만 출력 ->소수점 자리를 표시해주지 않는다
+		
+	}
+	
+}
