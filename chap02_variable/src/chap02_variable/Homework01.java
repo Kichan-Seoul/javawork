83087d081faebc2acb9f39c41ce20eb8a761a5b5
Kichan-Seoul <kimkc30@icloud.com>
2024-12-27 13:46:31 +0900

    Create Homework01.java

---
+++ chap02_variable/src/chap02_variable/Homework01.java
@@ -0,0 +1,21 @@
+package chap02_variable;
+
+public class Homework01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 = 3;
+		System.out.println("number1=" + number1);
+		int number2 = 5;
+		System.out.println("number2=" + number2);
+		int sum = number1 + number2;
+		System.out.println(number1 + "+" + number2 + "=" + sum);
+		int sub = number1 - number2;
+		int mul = number1 * number2;
+		int div = number1 / number2;
+		System.out.println(number1 + "-" + number2 + "=" + sub);
+		System.out.println(number1 + "*" + number2 + "=" + mul);
+		System.out.println(number1 + "/" + number2 + "=" + div);
+	}
+
+}
