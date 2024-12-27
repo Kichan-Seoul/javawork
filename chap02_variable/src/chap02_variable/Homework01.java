caa5cb633fad180acf03ed1e1440a66e20af2e7c
Kichan-Seoul <kimkc30@icloud.com>
2024-12-27 15:04:55 +0900

    Update Homework01.java

--- chap02_variable/src/chap02_variable/Homework01.java
+++ chap02_variable/src/chap02_variable/Homework01.java
@@ -3,11 +3,10 @@ package chap02_variable;
 public class Homework01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int number1 = 3;
-		System.out.println("number1=" + number1);
+		System.out.print("number1=" + number1);
 		int number2 = 5;
-		System.out.println("number2=" + number2);
+		System.out.println("\tnumber2=" + number2);
 		int sum = number1 + number2;
 		System.out.println(number1 + "+" + number2 + "=" + sum);
 		int sub = number1 - number2;
@@ -16,6 +15,7 @@ public class Homework01 {
 		System.out.println(number1 + "-" + number2 + "=" + sub);
 		System.out.println(number1 + "*" + number2 + "=" + mul);
 		System.out.println(number1 + "/" + number2 + "=" + div);
+		//sleeper.co.kr/product/%EB%88%84%EC%96%B4-%EB%A3%AC%EB%93%9C%EB%B9%85-%EC%88%98%EB%82%A9-%ED%98%B8%ED%85%94%ED%98%95-%EC%B9%A8%EB%8C%80%ED%94%84%EB%A0%88%EC%9E%84/3633/category/165/display/1/
 	}
 
 }
