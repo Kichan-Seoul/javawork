dfafe5bfc00c1ce6be75dddf1fcb5009b676b995
Kichan-Seoul <kimkc30@icloud.com>
2025-01-03 09:47:09 +0900

    Update T01_string.java

--- chap05_string/src/_01_string/T01_string.java
+++ chap05_string/src/_01_string/T01_string.java
@@ -18,9 +18,10 @@ public class T01_string {
 		System.out.println("그 안의 값이 같은가? " + str1.equals(str2));
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("주소 입력(시, 도까지만 : ");
+		System.out.println("주소 입력(시, 도까지만) : ");
 		String city = sc.next();
 		
+		// if(city=="경기도") -> city의 주소와 "경기도"가 같은가?
 		if(city.equals("경기도")) {
 			System.out.println("경기도사람");
 		}
